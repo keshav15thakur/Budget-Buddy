74f624e3f89e234a599d462a3290d6b3baf1fe0a
keshav thakur <168090970+keshav15thakur@users.noreply.github.com>
2025-06-28 00:09:20 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,83 @@
+const balance = document.getElementById('balance');
+const money_plus = document.getElementById('money-plus');
+const money_minus = document.getElementById('money-minus');
+const list = document.getElementById('list');
+const form = document.getElementById('form');
+const text = document.getElementById('text');
+const amount = document.getElementById('amount');
+
+let transactions = JSON.parse(localStorage.getItem('transactions')) || [];
+
+function addTransaction(e) {
+  e.preventDefault();
+
+  if (text.value.trim() === '' || amount.value.trim() === '') {
+    alert('Please add a description and amount');
+    return;
+  }
+
+  const transaction = {
+    id: generateID(),
+    text: text.value,
+    amount: +amount.value
+  };
+
+  transactions.push(transaction);
+  addToList(transaction);
+  updateValues();
+  updateLocalStorage();
+
+  text.value = '';
+  amount.value = '';
+}
+
+function generateID() {
+  return Math.floor(Math.random() * 1000000);
+}
+
+function addToList(transaction) {
+  const sign = transaction.amount < 0 ? '-' : '+';
+  const item = document.createElement('li');
+  item.classList.add(transaction.amount < 0 ? 'minus' : 'plus');
+  item.innerHTML = `
+    ${transaction.text} 
+    <span>${sign}₹${Math.abs(transaction.amount)}</span>
+    <button class="delete-btn" onclick="removeTransaction(${transaction.id})">x</button>
+  `;
+  list.appendChild(item);
+}
+
+function updateValues() {
+  const amounts = transactions.map(trans => trans.amount);
+  const total = amounts.reduce((acc, val) => acc + val, 0).toFixed(2);
+  const income = amounts
+    .filter(val => val > 0)
+    .reduce((acc, val) => acc + val, 0)
+    .toFixed(2);
+  const expense = (
+    amounts.filter(val => val < 0).reduce((acc, val) => acc + val, 0) * -1
+  ).toFixed(2);
+
+  balance.innerText = `₹${total}`;
+  money_plus.innerText = `+₹${income}`;
+  money_minus.innerText = `-₹${expense}`;
+}
+
+function removeTransaction(id) {
+  transactions = transactions.filter(trans => trans.id !== id);
+  updateLocalStorage();
+  init();
+}
+
+function updateLocalStorage() {
+  localStorage.setItem('transactions', JSON.stringify(transactions));
+}
+
+function init() {
+  list.innerHTML = '';
+  transactions.forEach(addToList);
+  updateValues();
+}
+
+init();
+form.addEventListener('submit', addTransaction);
